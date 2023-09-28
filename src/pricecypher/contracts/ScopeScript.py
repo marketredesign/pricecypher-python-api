bfca4320ceeec96083e15890a6c3825fc6f6f267
Marijn van der Horst <marvanderhorst@deloitte.nl>
2023-09-28 17:05:58 +0200

    BREAKING CHANGE: Update Script contracts to take `get_bearer_token` function argument instead of static `bearer_token`
    sem-ver: api-break

--- src/pricecypher/contracts/ScopeScript.py
+++ src/pricecypher/contracts/ScopeScript.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any, Optional
+from typing import Any, Optional, Callable
 
 from pricecypher.contracts.Script import Script
 
@@ -11,26 +11,31 @@ class ScopeScript(Script, ABC):
         which can then be used in a generalized yet controlled setting.
     """
 
-    def execute(self, business_cell_id: Optional[int], bearer_token: str, user_input: dict[Any: Any]) -> Any:
+    def execute(
+        self,
+        business_cell_id: Optional[int],
+        get_bearer_token: Callable[[], str],
+        user_input: dict[Any: Any],
+    ) -> Any:
         # Executing a scope-script like a normal script:
         # Attempt to extract the transaction IDs and continue as normal
         if 'transaction_ids' not in user_input:
             raise Exception('Expected input key "transaction_ids" not present.')
         transaction_ids: list[int] = user_input['transaction_ids']
-        return self.execute_scope_script(business_cell_id, bearer_token, transaction_ids)
+        return self.execute_scope_script(business_cell_id, get_bearer_token, transaction_ids)
 
     @abstractmethod
     def execute_scope_script(
         self,
         business_cell_id: Optional[int],
-        bearer_token: str,
-        transaction_ids: list[int]
+        get_bearer_token: Callable[[], str],
+        transaction_ids: list[int],
     ) -> dict[int, str]:
         """
         Execute the script to calculate the values of some scope for the given transactions.
 
         :param business_cell_id: Business cell to execute the script for, or None if running the script for all.
-        :param bearer_token: Bearer token to use for additional requests.
+        :param get_bearer_token: Function that can be invoked to retrieve an access token.
         :param transaction_ids: List of transaction IDs to calculate the scope values/constants for.
         :return: Dictionary mapping every transaction ID to a string value for the scope of this script.
         """
