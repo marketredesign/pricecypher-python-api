6bcdb9bfeb40577ff9dacd7d3d52382304e9a143
Marijn van der Horst <marvanderhorst@deloitte.nl>
2024-05-24 23:35:18 +0200

    Update custom exceptions

--- src/pricecypher/exceptions.py
+++ src/pricecypher/exceptions.py
@@ -4,9 +4,11 @@ import warnings
 class HttpException(Exception):
     def __init__(self, message: str, **kwargs):
         super().__init__(message)
+
         self.message = kwargs.get('message', message)
         self.status_code = kwargs.get('status_code', 500)
         self.code = kwargs.get('error_code', "Internal Server Error")
+        self.extra = kwargs.get('extra')
 
     def __str__(self):
         return f'{self.status_code} {self.code}: {self.message}'
@@ -19,7 +21,8 @@ class HttpException(Exception):
                 'x-amzn-ErrorType': self.code,
             },
             'isBase64Encoded': False,
-            'body': f'{self.code}: {str(self)}'
+            'body': f'{self.code}: {str(self)}',
+            'extra': self.extra,
         }
 
 
@@ -27,14 +30,6 @@ class PriceCypherError(HttpException):
     def __init__(self, status_code, error_code, message):
         warnings.warn('Use of the class `PriceCypherError` is deprecated. Please use `HttpException` instead.')
         super().__init__(message, status_code=status_code, error_code=error_code)
-
-
-class RateLimitError(HttpException):
-    def __init__(self, message: str, **kwargs):
-        super().__init__(message, **kwargs)
-        self.status_code = 429
-        self.code = kwargs.get('error_code', 'Too Many Requests')
-        self.reset_at = kwargs.get('reset_at')
 
 
 class MissingInputException(HttpException):
@@ -46,19 +41,35 @@ class MissingInputException(HttpException):
         message -- explanation of the error
     """
 
-    def __init__(self, scopes, business_cell=False, message="MissingInputException: Missing input variables: "):
+    def __init__(self, message: str, **kwargs):
+        super().__init__(message, **kwargs)
+        scopes: list[str] = kwargs.get('scopes', [])
+
+        self.statusCode = 400
+        self.code = kwargs.get('error_code', 'Bad Request')
+        self.code = 'Bad Request'
+        self.message = f'{message or "Missing input variables:"} {", ".join(scopes)}'
+        self.extra = {'scopes': kwargs.get('scopes')}
+
+
+class IncorrectVolumeException(HttpException):
+    """Exception raised when user input has incorrect volume.
+
+    Attributes:
+        val -- incorrect volume value
+        message -- explanation of the error
+    """
+
+    def __init__(self, message: str, **kwargs):
+        super().__init__(self.message)
+        val = kwargs.get('val')
+
         self.statusCode = 400
         self.code = 'Bad Request'
-        self.scopes = scopes
-        self.business_cell = business_cell
-        self.message = message
-        super().__init__(self.message)
-
-    def __str__(self):
-        if self.business_cell:
-            return self.message + self.scopes + ". Please provide business cell input."
-        else:
-            return self.message + ", ".join(self.scopes)
+        self.message = f'''
+            {message or "IncorrectVolumeException: Incorrect volume entered. Please enter positive value"} 
+            ({val})'message + str(val) + ". Please enter a positive value."'''
+        self.extra = {'volume': val}
 
 
 class DataNotFoundException(HttpException):
@@ -70,34 +81,15 @@ class DataNotFoundException(HttpException):
         message -- explanation of the error
     """
 
-    def __init__(self, key, value, message="DataNotFoundException: Data point not found in dataset for column "):
+    def __init__(self, message: str, **kwargs):
+        super().__init__(self.message)
+        key = kwargs.get('key', "Unknown")
+        value = kwargs.get('value', "Unknown")
+
         self.statusCode = 404
         self.code = 'Not Found'
-        self.key = key
-        self.value = value
-        self.message = message + str(key) + ": " + str(value)
-        super().__init__(self.message)
-
-    def __str__(self):
-        return self.message
-
-
-class IncorrectVolumeException(HttpException):
-    """Exception raised when user input has incorrect volume.
-
-    Attributes:
-        val -- incorrect volume value
-        message -- explanation of the error
-    """
-
-    def __init__(self, val, message="IncorrectVolumeException: Incorrect volume entered: "):
-        self.statusCode = 400
-        self.code = 'Bad Request'
-        self.message = message + str(val) + ". Please enter a positive value."
-        super().__init__(self.message)
-
-    def __str__(self):
-        return self.message
+        self.message = f'{message or "Data point not found in dataset for column"} {key}: {value}.'
+        self.extra = {'key': key, 'value': value}
 
 
 class MissingRepresentationException(HttpException):
@@ -109,12 +101,20 @@ class MissingRepresentationException(HttpException):
         message -- explanation of the error
     """
 
-    def __init__(self, val, message="MissingRepresentationException: "):
+    def __init__(self, message: str, **kwargs):
+        super().__init__(self.message)
+        val = kwargs.get('val')
+
         self.statusCode = 409
         self.code = 'Conflict'
-        self.message = message + f""""{val} column is not a representation. 
-        Please update scopes file to include a scope with the representation."""
-        super().__init__(self.message)
+        self.message = f'{message or "Unable to find representation. Please update scopes file."} ({val})'
+        self.extra = {'column': val}
 
-    def __str__(self):
-        return self.message
+
+class RateLimitError(HttpException):
+    def __init__(self, message: str, **kwargs):
+        super().__init__(message, **kwargs)
+
+        self.status_code = 429
+        self.code = kwargs.get('error_code', 'Too Many Requests')
+        self.extra = {'reset_at': kwargs.get('reset_at')}
