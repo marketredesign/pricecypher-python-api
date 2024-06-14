ba0be2e26908d92f162dfc87efeff4b30648a4fb
Marijn van der Horst <marvanderhorst@deloitte.nl>
2024-06-14 09:52:29 +0200

    Add docs to `AccessTokenGrantType` enum

--- src/pricecypher/enums/access_token_grant_type.py
+++ src/pricecypher/enums/access_token_grant_type.py
@@ -5,6 +5,12 @@ from pricecypher.oidc.auth import StaticTokenGenerator, ClientTokenGenerator
 
 
 class AccessTokenGrantType(Enum):
+    """
+    We refer to a 'static' access token when the token does not change during a single execution of an event handler.
+    The 'client_credentials' grant type can be used by event handlers that can potentially run for a longer time
+    than the lifetime of a single access token. Though, it may not be used by event handlers that are triggered by a
+    user directly.
+    """
     STATIC = 'static'
     CLIENT_CREDENTIALS = 'client_credentials'
 
