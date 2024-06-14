20700dc78a1db6c875f9108096a1878ea6fa2cf6
Marijn van der Horst <marvanderhorst@deloitte.nl>
2024-06-14 09:41:20 +0200

    Fix incorrect PyDoc

--- src/pricecypher/contracts/dataclasses/predict_result.py
+++ src/pricecypher/contracts/dataclasses/predict_result.py
@@ -21,16 +21,11 @@ class PredictStep:
 @dataclass
 class PredictResult:
     """
-    One quality test script always produces one TestSuite response. A test suite (usually) contains multiple test cases.
-    It also defines a category that can be used by front-ends to group multiple test suites together.
+    Result after model inference, i.e. the model prediction result. It consists of the following properties.
 
-    label (str): Label of the test suite, e.g. 'Completeness'.
-
-    key (str): Unique identifier of the test suite (lowercase kebab-case), e.g. 'basic-completeness'.
-
-    category_key (str): Unique identifier of the category this test suite is in, e.g. 'basic' or 'advanced'.
-
-    test_results (list[TestResult]): All test cases of this test suite, with their results.
+    - `predictive_values`: The value and associated min-max range that form the of the underlying prediction.
+    - `accuracy`: The accuracy of the underlying prediction.
+    - `predictive_steps`: The different (pricing) steps building up the total underlying prediction.
     """
     predictive_values: PredictValues
     predictive_steps: list[PredictStep]
