27393793a4f51780bf1024b72713281afd6c5641
Marijn van der Horst <marvanderhorst@deloitte.nl>
2024-08-22 23:26:01 +0200

    Add ScriptService to top-level __init__

--- src/pricecypher/__init__.py
+++ src/pricecypher/__init__.py
@@ -3,8 +3,9 @@ import pbr.version
 from .collections import ScopeCollection, ScopeValueCollection
 from .config_sections import ConfigSections
 from .datasets import Datasets
+from .script_service import ScriptService
 from .rest import RestClient
 
-__all__ = ['ScopeCollection', 'ScopeValueCollection', 'ConfigSections', 'Datasets', 'RestClient']
+__all__ = ['ScopeCollection', 'ScopeValueCollection', 'ConfigSections', 'Datasets', 'RestClient', 'ScriptService']
 
 __version__ = pbr.version.VersionInfo('pricecypher_sdk').version_string()
